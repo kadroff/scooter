6096bc8792eba34c7565a53221064170931decd6
Marsel Kadrov <marselkadroff@gmail.com>
2016-06-06 23:31:59 +0500

    Небольшие доработки

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="js/bootstrap.min.js" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+
     <script src="js/jquery-2.2.3.min.js"></script>
     <script src="js/app.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
 <body>
     <div class="container">
+      <div class="header">
       <div class="row">
         <div class="navbar navbar-inverse navbar-fixed-top">
           <div class="container">
@@ -31,8 +43,9 @@
       </div>
     </div>
   </div>
+</div>
 
-      
+    <div class="container">
       <div class="main-block">
           <p class="welcome-text">Мы поможем тебе подобрать любые запчасти для твоего двухколесного друга</p>
           <!-- <img src="image/Без имени.png"></img> -->
@@ -41,8 +54,8 @@
           </form>
 
       </div>
-    </div>
-  </div>
+</div>
+
 
 
 </body>
--- scripts/engine.php
+++ scripts/engine.php
@@ -18,23 +18,27 @@
   <head>
       <meta charset="utf-8">
       <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/bootstrap.css" rel="stylesheet">
       <link href="js/bootstrap.min.js" rel="stylesheet">
       <link href="css/main.css" rel="stylesheet">
       <script src="js/jquery-2.2.3.min.js"></script>
       <script src="js/app.js"></script>
   </head>
     <body>
-       <ul class="select-engine-left">
-         <li id="minarelli-h"><a>Minarelli (horizontal)</a></li>
-         <li id="minarelli-v">Minarelli (vertical)</li>
-         <li id="3kj">3kj</li>
-         <li id="hiper">Hi-per-2pro</li>
-       </ul>
+      <div class="container">
+         <ul class="select-engine-left">
+           <li id="minarelli-h"><a>Minarelli (horizontal)</a></li>
+           <li id="minarelli-v">Minarelli (vertical)</li>
+           <li id="3kj">3kj</li>
+           <li id="hiper">Hi-per-2pro</li>
+         </ul>
 
-        <ul class="select-engine-right">
-          <li id="af18">Af 18/27</li>
-          <li id="af34">Af 34-35</li>
-          <li id="morini">Morini</li>
-          <li id="bigblock">Hi-per bigblock</li>
+          <ul class="select-engine-right">
+            <li id="af18">Af 18/27</li>
+            <li id="af34">Af 34-35</li>
+            <li id="morini">Morini</li>
+            <li id="bigblock">Hi-per bigblock</li>
+          </ul>
+      </div>
    </body>
    </html>
