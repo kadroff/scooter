a98475d4f30807fd01fa6bfbb90a5b806a70de7f
Marsel Kadrov <marselkadroff@gmail.com>
2016-06-24 16:20:56 +0500

    не получается верный вывод

--- index.php
+++ index.php
@@ -10,10 +10,20 @@
 </head>
 <body>
   <ul>
-   <a href="motors.php?id=1"><?php foreach ($sql as $value) {
-    echo "<li>". $value['name']. "</li>";
-      
-    } ?></a>
+   <a href="motors.php?id=1"><?php
+   foreach ($sql as $value) {
+     $row = mysqli_fetch_array($sql, MYSQLI_ASSOC);
+        printf ("%s (%s)\n", $row["name"]);
+        echo $row('name');
+      }
+
+
+
+  //  foreach ($sql as $value) {
+  //   echo "<li>". $value['name']. "</li>";
+  //   // var_dump($value);
+
+    ?></a>
     <li><a href="motors.php?id=2"></li></a>
     <li><a href="motors.php?id=3"></li></a>
 
